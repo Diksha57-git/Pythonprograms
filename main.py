91e30416acebd43e866d9d19e1b2ad4bc38c5279
Diksha57-git <dikshatan0909@gmail.com>
2026-01-07 10:39:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy
+a=5
+b=7
+c=a+b
+print("First num:",a)
+print("Second num:",b)
+print("Addition:",c)
+print("Substraction:",(a-b))
+print("Division:",(a/b))
+print("Multiplication:",(a*b))
+print("modulas:",(a%b))
+
